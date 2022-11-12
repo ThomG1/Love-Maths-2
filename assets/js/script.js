65704b3daf2097d0664741cb44d0444fbc303338
ThomG1 <thomas.gwyther@hotmail.co.uk>
2022-11-12 11:31:32 +0000

    Add calculating correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -34,6 +34,15 @@ function runGame (gameType) {
 
 function checkAnswer() {
 
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert ("Hey! You got it right! :D");
+    } else {
+        alert (`Awww.... You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
 }
 
 function calculateCorrectAnswer() {
@@ -48,6 +57,8 @@ function calculateCorrectAnswer() {
         alert (`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
+
+    runGame(calculatedAnswer [1]);
 
 }
 
