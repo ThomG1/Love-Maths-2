eb3bc6113a5f13bb5657582e64d2e91a95a594df
ThomG1 <thomas.gwyther@hotmail.co.uk>
2022-11-12 10:24:38 +0000

    Add correct answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,25 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
-function runGame () {
+function runGame (gameType) {
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 2;
+
+    if (gameType == "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert (`Unknown game type:  ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting`;
+    }
 
 }
 
@@ -28,6 +37,17 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById(operand1).innerText);
+    let operand2 = parseInt(document.getElementById(operand2).innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert (`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
@@ -39,8 +59,11 @@ function incrementWrongAnswers() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
